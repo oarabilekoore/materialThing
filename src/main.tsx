82ba48ca8fd39dc86bf865a0a910ecdb14f5c7ab
oarabilekoore <oarabilekoore@protonmail.com>
2025-09-25 18:27:52 +0200

    fixed jsx

--- src/main.tsx
+++ src/main.tsx
@@ -9,18 +9,16 @@ function App() {
 
   return (
     <div
-      class={
-        css({
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100vh",
-          gap: "20px",
-          backgroundColor: "var(--md-sys-color-background)",
-          color: "var(--md-sys-color-on-background)",
-        }) + " dark"
-      }
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        gap: "20px",
+        backgroundColor: "var(--md-sys-color-background)",
+        color: "var(--md-sys-color-on-background)",
+      }}
     >
       <div
         style={{
